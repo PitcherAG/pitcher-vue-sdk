c2f5a81423fd447b678b984ed1467c231d49dc67
Onur Elibol <onur.elibol@pitcher.com>
2020-10-26 10:09:08 +0100

    fix export

--- src/components/export.js
+++ src/components/export.js
@@ -1,7 +1,7 @@
 /* Here you have to export all the components you have created */
 
 // Vue Components
-export { default as OldCalendar } from './OldCalendar.vue'
+export { default as Calendar } from './Calendar.vue'
 export { default as Checkbox } from './Checkbox.vue'
 export { default as DataTable } from './DataTable/DataTable.vue'
 export { default as Dropdown } from './Dropdown.vue'
@@ -12,6 +12,7 @@ export { default as Modal } from './Modal.vue'
 export { default as NumpadInput } from './NumpadInput.vue'
 export { default as ObjectForm } from './ObjectForm.vue'
 export { default as ObjectFormField } from './ObjectFormField.vue'
+export { default as OldCalendar } from './OldCalendar.vue'
 export { default as Popup } from './Popup.vue'
 export { default as ProgressBar } from './ProgressBar.vue'
 export { default as Sidebar } from './Sidebar.vue'
